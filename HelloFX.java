14eabeeeb7d05a18f1762f5cb21c4a7f0b828391
Reichidad <47580804+Reichidad@users.noreply.github.com>
2019-05-13 22:48:49 +0900

    Update HelloFX.java

--- HelloFX.java
+++ HelloFX.java
@@ -9,13 +9,17 @@ public class HelloFX extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
         stage.show();
-
+        stage.do();
+        stage.do();
+        stage.do();
+        stage.do();
+        
+        stage.do();
         //addOneLine
         stage.show();
+         stage.do();
     }
     public static void main(String[] args) {
         launch();
